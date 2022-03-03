2142c21a513a898e69d609d7b585616c4c5b5283
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-03-03 09:05:27 +0500

    Added FizzBuzz

--- js/main.js
+++ js/main.js
@@ -315,3 +315,15 @@ for(let i = a; i <= b; i++){
   a++;
 }
 */
+
+// FizzBuzz
+
+let a = 12;
+
+if(a % 3 == 0 && a % 5 == 0){
+  console.log("FizzBuzz");
+} else if (a % 3 == 0){
+  console.log("Fizz");
+} else {
+  console.log("Buzz");
+}
